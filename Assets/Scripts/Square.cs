20da2e63ec8e36bf3686dbc63ec41879794f13e2
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2021-10-27 12:35:24 +0000

    Revert "Update Square.cs"
    This reverts commit 4a31f4c955b8a029045ffdfe012e706462949019.

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -34,23 +34,67 @@ public class Square : MonoBehaviour
 	private void Start()
 	{
 		//Applying column and row number 
-		if(transform.position.x > 35f || transform.position.x < -35f || transform.position.x + 5 % 10 != 0)
+		switch (transform.position.x)
 		{
-			Debug.LogError("Square's x position is not correct");
+			case -35f:
+				column = 0;
+				break;
+			case -25f:
+				column = 1;
+				break;
+			case -15f:
+				column = 2;
+				break;
+			case -5f:
+				column = 3;
+				break;
+			case 5f:
+				column = 4;
+				break;
+			case 15f:
+				column = 5;
+				break;
+			case 25f:
+				column = 6;
+				break;
+			case 35f:
+				column = 7;
+				break;
+			default:
+				Debug.LogError("Square's x position is not correct");
+				break;
 		}
-		else{
-			column = (transform.position.x + 35f) / 10f;
-		}
-			
 
-		if(transform.position.y > 35f || transform.position.y < -35f || transform.position.y + 5 % 10 != 0)
+		switch (transform.position.y)
 		{
-			Debug.LogError("Square's y position is not correct");
+			case -35f:
+				row = 0;
+				break;
+			case -25f:
+				row = 1;
+				break;
+			case -15f:
+				row = 2;
+				break;
+			case -5f:
+				row = 3;
+				break;
+			case 5f:
+				row = 4;
+				break;
+			case 15f:
+				row = 5;
+				break;
+			case 25f:
+				row = 6;
+				break;
+			case 35f:
+				row = 7;
+				break;
+			default:
+				Debug.LogError("Square's y position is not correct");
+				break;
 		}
-		else{
-			row = (transform.position.y + 35f) / 10f;
-		}
-			
 	}
 
 	private void FixedUpdate()
